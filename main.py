a0aa6ce5cc756a38847d11fe34bd79d6dcf8e817
Edoardo <edoardore@me.com>
2020-11-14 16:57:55 +0100

    commit

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ wheelDx = Motor(OUTPUT_A)
 wheelSx = Motor(OUTPUT_D)
 motor = MoveTank(OUTPUT_A, OUTPUT_D)
 gyro = GyroSensor(INPUT_4)
+PI = 3.1416
 
 
 def getGyroRate(readings):
@@ -30,77 +31,68 @@ def calibrate_gyro(loops):
     return mean
 
 
-def sensor(Offset_gyro):
-    angleDx = wheelDx.position+120
-    angleSx = wheelSx.position+120
+def sensor(Offset_gyro, ThetaTimeList, Psi_dotTimeList):
+    angleDx = wheelDx.position
+    angleSx = wheelSx.position
+    t = time()
+    ThetaTimeList.append(t)
     ThetaM = (angleDx+angleSx)/2
-    ThetaM = ThetaM*3.1416/180
+    ThetaM = ThetaM*PI/180
     Psi_dot = gyro.rate
-    Psi_dot = Psi_dot*3.1416/180
+    t = time()
+    Psi_dotTimeList.append(t)
+    Psi_dot = Psi_dot*PI/180
     Psi_dot -= Offset_gyro
-    print('Sensor ThetaM is '+str(ThetaM) +
-          '\nSensor psi_dot is '+str(Psi_dot), file=stderr)
+    '''print('Sensor ThetaM is '+str(ThetaM) +
+         '\nSensor psi_dot is '+str(Psi_dot), file=stderr)
+    '''
     return (ThetaM, Psi_dot)
 
 
-def integrate(List, Ti, Tf):
-    i = Ti
+def integrate(List, TimeList):
     s = 0
-    Ts = 0.05
-    j = 0
-    while i <= Tf and j < len(List):
+    j = 1
+    while j < len(List):
+        Ts = TimeList[j]-TimeList[j-1]
         s += List[j]*Ts
-        i += Ts
         j += 1
     return s
 
 
-def derivativeDiscrete(L, x):
-    # t=(tf-ti)/len(L)
-    t = 0.05
-    if len(L) == 1:
-        return L[0]/(2*t)
-    else:
-        return (L[x+1] - L[x-1]) / (2*t)
-
-
-def observer(Psi_dot, Theta, Offset_gyro):
-    # Psi_dot -= Offset_gyro
-    #Theta_dot = (wheelDx.speed+wheelSx.speed)/2
-    #Theta_dot = Theta_dot*3.1416/180
-    # print('Observer Theta is '+str(Theta) +
-    #      '\nObserver Theta_dot is '+str(Theta_dot)+'\nObserver Psi_dot '+str(Psi_dot), file=stderr)
-    # return(Theta, Theta_dot, Psi_dot)
-    return(Psi_dot)
+def derivativeDiscrete(L, x, TimeList):
+    t = TimeList[x]-TimeList[x-1]
+    return (L[x] - L[x-1]) / t
 
 
 def control(Theta, Psi, Theta_dot, Psi_dot, Theta_int):
-    K = [[-0.855, - 44.7896, - 0.9936, - 4.6061, -0.5000],
-         [-0.855, - 44.7896, - 0.9936, - 4.6061, - 0.5000]]
+    K = [[-0.8559, - 44.7896, - 0.9936, - 4.6061, -0.5000],
+         [-0.8559, - 44.7896, - 0.9936, - 4.6061, - 0.5000]]
     x = [[Theta], [Psi], [Theta_dot], [Psi_dot], [Theta_int]]
     u = [[0], [0]]
     for i in range(0, 2):
         j = 0
         for k in range(0, 5):
             u[i][j] += K[i][k]*x[k][j]
-    print(u, file=stderr)
+    #print(u, file=stderr)
     return u
 
 
+def control2(Theta, Psi, Theta_dot, Psi_dot, Theta_int):
+    u = (-0.8559*Theta)+(-44.7896*Psi)+(-0.9936*Theta_dot)+(-4.6061*Psi_dot)+(-0.500*Theta_int)
+
+
 def rackUp(r):
-    r.on_for_rotations(25, 0.30)
+    r.run_to_rel_pos(position_sp=90, speed_sp=500, stop_action="brake")
 
 
 def rackDown(r):
-    r.on_for_rotations(50, -0.30)
+    r.run_to_rel_pos(position_sp=-90, speed_sp=500, stop_action="brake")
 
 
 def main():
-    offset = calibrate_gyro(5)*3.1416/180
+    offset = calibrate_gyro(5)*PI/180
     print("Offset is "+str(offset), file=stderr)
     sleep(1)
-    motor.on_for_degrees(20, 20, -120, brake=True, block=True)
-    rackUp(rack)
     wheelSx.stop()
     wheelDx.run_direct()
     wheelSx.stop()
@@ -108,31 +100,25 @@ def main():
     u = [[]]
     ThetaList = []
     Psi_dotList = []
-    millisecondsStart = time()
-    ThetaList.append(0)
-    Psi_dotList.append(0)
-    PI = 3.1416
+    ThetaTimeList = []
+    Psi_dotTimeList = []
     count = 0
+    millisecondsStart = time()
     while True:
         t = time()
         if (t-millisecondsStart) > 10:
             break
-        (ThetaM, Psi_dot) = sensor(offset)
-        #(Theta, Theta_dot, Psi_dot) = observer(Psi_dot, Theta, offset)
-        Psi_dotList.append(Psi_dot)
-        Psi = integrate(Psi_dotList, millisecondsStart, t)
-        print("Observer Psi is "+str(Psi), file=stderr)
-        Theta = ThetaM+Psi
-        if Theta < 0:
-            Theta %= (-2*PI)
-        else:
-            Theta %= (2*PI)
-        print("Observer Theta is "+str(Theta), file=stderr)
-        ThetaList.append(Theta)
-        Theta_dot = derivativeDiscrete(ThetaList, len(ThetaList)-2)
-        print("Observer Theta_dot is "+str(Theta_dot), file=stderr)
-        Theta_int = integrate(ThetaList, millisecondsStart, t)
-        print("Theta_int is "+str(Theta_int), file=stderr)
+        if count == 0:
+            Theta = 0
+            Psi = -8.7*PI/180
+            Theta_dot = 0
+            Psi_dot = 0
+            Theta_int = 0
+            ThetaList.append(0)
+            Psi_dotList.append(0)
+            t1 = time()
+            ThetaTimeList.append(t1)
+            Psi_dotTimeList.append(t1)
         u = control(Theta, Psi, Theta_dot, Psi_dot, Theta_int)
         count += 1
         Vr = u[1][0]
@@ -143,12 +129,23 @@ def main():
             Vr = -100
         wheelDx.duty_cycle_sp = Vr
         wheelSx.duty_cycle_sp = Vr
-        print(count, file=stderr)
+        if count == 1:
+            rackUp(rack)
+        (ThetaM, Psi_dot) = sensor(offset, ThetaTimeList, Psi_dotTimeList)
+        Psi_dotList.append(Psi_dot)
+        Psi = integrate(Psi_dotList, Psi_dotTimeList)
+        #print("Observer Psi is "+str(Psi), file=stderr)
+        Theta = ThetaM+Psi
+        #print("Observer Theta is "+str(Theta), file=stderr)
+        ThetaList.append(Theta)
+        Theta_dot = derivativeDiscrete(ThetaList, len(ThetaList)-1, ThetaTimeList)
+        #print("Observer Theta_dot is "+str(Theta_dot), file=stderr)
+        Theta_int = integrate(ThetaList, ThetaTimeList)
+        #print("Theta_int is "+str(Theta_int), file=stderr)
+    print(count, file=stderr)
+    wheelSx.stop()
+    wheelDx.stop()
     rackDown(rack)
 
 
 main()
-
-
-#TODO tempi per integrale e derivate attenzione a conversione ms.
-#implementare aggiunta -8.7 per lo psi
